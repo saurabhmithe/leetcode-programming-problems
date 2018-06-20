c379f8907eaf7ca4bc87bfbb72222138fa56a7f4
Saurabh Mithe <saurabhmithe@gmail.com>
2018-06-20 02:05:01 -0700

    added easy HashTable problem appraoch

--- Solutions/src/HashTable/Easy/SingleNumber.java
+++ Solutions/src/HashTable/Easy/SingleNumber.java
@@ -1,5 +1,7 @@
 package HashTable.Easy;
 
+import java.util.Hashtable;
+
 public class SingleNumber {
 
     /*
@@ -40,6 +42,35 @@ public class SingleNumber {
             result = result ^ nums[i];
         }
         return result;
+    }
+
+
+    /**
+     * This approach uses HashTable since it is one of the mainstream ways of dealing with counting problems.
+     * HashTable allows constant time lookup which is a desirable property.
+     * We store the element as the key and the number of times it appears as the value.
+     * In the end, each element would have a value of 2 except for that one single element.
+     * We would need another pass over the HashTable to figure out which one it was.
+     * <p>
+     * Time Complexity - O(2n) => O(n)
+     * Space Complexity - O(n)
+     * <p>
+     * Runtime on LeetCode - 22ms
+     * Runtime beats 11.68% of Java submissions
+     */
+    public int singleNumberHashTable(int[] nums) {
+        Hashtable<Integer, Integer> hashTable = new Hashtable<>();
+        for (int num : nums) {
+            if (!hashTable.containsKey(num)) {
+                hashTable.put(num, 1);
+            } else {
+                hashTable.put(num, hashTable.get(num) + 1);
+            }
+        }
+        for (int num : nums) {
+            if (hashTable.get(num) == 1) return num;
+        }
+        return 0;
     }
 
 }
